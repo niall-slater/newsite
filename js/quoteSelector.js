b7e0d386f6e9bb27864d1f0eb4925edda6d52ac5
Niall Slater <niallslater@gmail.com>
2018-04-20 10:15:01 +0100

    Added random quote selector

---
+++ js/quoteSelector.js
@@ -0,0 +1,47 @@
+var quotes = [
+    {
+        "quote": "Slater has not just redefined the novel but every single word within the novel.",
+        "attribution": "Dave Probert"
+    },
+    {
+        "quote": "Had me completely hooked, right up to the first page.",
+        "attribution": "Stevyn Colgan"
+    },
+    {
+        "quote": "Not so much a novel as a note-perfect meditation on what the novel could become.",
+        "attribution": "Mathew Lyons"
+    },
+    {
+        "quote": "Heartfelt, fragrant, and ultimately worth the lives of the many, many emus who were tragically but necessarily destroyed to make it.",
+        "attribution": "@iucounu"
+    },
+    {
+        "quote": "Every sentence is lovingly hand-crafted and comes with a commemorative plate of the late Queen Mother.",
+        "attribution": "John Dougherty"
+    },
+    {
+        "quote": "Slater combines the eloquence of words and the silence of punctuation into a book of significant pages that run from beginning to end.",
+        "attribution": "Justin K. Hayward"
+    },
+    {
+        "quote": "I found the page numbers strangely compelling.",
+        "attribution": "Jonathan L. Howard"
+    },
+    {
+        "quote": "Before the first page, I was sweating. By the second page, I had soaked the first page. A brilliantly absorbing novel.",
+        "attribution": "Alex Christofi"
+    },
+    {
+        "quote": "Niall's book is definitely the third-best millipede-rearing instruction manual in my collection, hands down.",
+        "attribution": "Kim Staples"
+    },
+    {
+        "quote": "A stunning debut...Slater builds worlds with ease & his complex characterisation will suck you in like the gravity on [insert planet here]",
+        "attribution": "Becca Wright"
+    }
+];
+
+var selectedQuote = quotes[Math.floor(Math.random()*quotes.length)];
+
+document.getElementById("quoteHolder").innerHTML = '"' + selectedQuote.quote + '"';
+document.getElementById("attributionHolder").innerHTML = selectedQuote.attribution;
